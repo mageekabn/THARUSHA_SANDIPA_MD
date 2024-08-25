ca085914f9950f6689483dd6ea54d697e1de8bae
𝗧𝗛𝗔𝗥𝗨𝗦𝗛𝗔 𝗦𝗔𝗡𝗗𝗜𝗣𝗔 <166241288+mageekabn@users.noreply.github.com>
2024-08-25 15:51:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/8145c950168984437d87e.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello,I am Tharusha sandipa I am alive now💚",
 };
